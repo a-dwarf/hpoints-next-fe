30a4f0e6f8673b147e027547e8a4df24904b120e
haike <haike0513@gmail.com>
2024-06-21 14:56:25 +0800

    upgrade for schema

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function getUserId(address: any): Promise<number | null> {
+export async function getUserId(address: any){
   try {
     const user = await prisma.user.findUnique({
       where: { address: address },
